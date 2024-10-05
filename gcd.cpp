0eaa3c5566166872d82d4047ca16a602429849f3
Panda-Ani <95610095+Panda-Ani@users.noreply.github.com>
2024-10-06 03:25:44 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(a,a%b);
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b)<<endl;
+}
