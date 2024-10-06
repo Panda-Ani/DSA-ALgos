404f364455c1adaf8a7585da2570d087c162cde9
Panda-Ani <95610095+Panda-Ani@users.noreply.github.com>
2024-10-06 17:18:08 +0530

    Coordinate_Compression.cpp

---
+++ Coordinate_Compression.cpp
@@ -0,0 +1,39 @@
+class CoordinateCompression{
+    map<long long, int> mp;
+    vector<int> rev;
+    int cnt = 1;
+
+    public:
+        void insert(long long num)
+        {
+            mp[num] = 0;
+        }
+
+        void build()
+        {
+            for(auto &map : mp)
+            {
+                map.second=cnt++;
+                rev.push_back(map.first);
+            }
+        }
+
+        int index(long long num)
+        {
+            if(mp.find(num) != mp.end())return mp[num];
+            else return -1;
+        }
+
+        int size()
+        {
+            return cnt;
+        }
+
+        int IndexOriginal(int ind)
+        {
+            if(ind < cnt)return rev[ind];
+            return -1;
+        }
+
+
+};
