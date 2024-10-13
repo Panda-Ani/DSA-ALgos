b1ee24f20cfe092a85595f88b5fca20116532066
Panda-Ani <95610095+Panda-Ani@users.noreply.github.com>
2024-10-13 19:53:05 +0530

    Update TopoSort.cpp

--- TopoSort.cpp
+++ TopoSort.cpp
@@ -52,7 +52,7 @@ int main()
         int u,v;
         cin>>u>>v;
         adj[u].push_back(v);
-        adj[v].push_back(u);
+        // adj[v].push_back(u);
     }
     
     vector<int> ans = topo(n,adj);
