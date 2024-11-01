d9ff0269f2ce3f43e6602c6b27c80dd91b86e334
Panda-Ani <95610095+Panda-Ani@users.noreply.github.com>
2024-11-01 14:11:51 +0530

    Add files via upload

---
+++ Tarjan_Bridges_Algo.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void dfs(int node, int par, vector<int> &vis, vector<int> adj[], vector<int> &tin,
+        vector<int> &low, vector<vector<int> > &bridges, int &timer)
+    {
+        vis[node] = 1;
+        tin[node] = low[node] = timer++;
+
+        for(auto &it : adj[node])
+        {
+            if(it == par)continue;
+            if(!vis[it])
+            {
+                dfs(it,node,vis,adj,tin,low,bridges,timer);
+                low[node] = min(low[node],low[it]); // check for lowest time in adj
+                if(low[it] > tin[node]) // it can't be reached by node
+                {
+                    bridges.push_back({node,it});
+                }
+            }
+            else
+            {
+                low[node] = min(low[node],low[it]);
+            }
+        }
+    }
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int> > edges;
+
+    for(int i = 0; i < m; i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        edges.push_back({u,v});
+    }
+
+    vector<int> adj[n+1];
+
+    for(auto &it : edges)
+    {
+        adj[it[0]].push_back(it[1]);
+        adj[it[1]].push_back(it[0]);
+    }
+
+    vector<int> tin(n+1,0), low(n+1,0), vis(n+1,0);
+
+    int timer = 1;
+
+    vector<vector<int> > bridges;
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(!vis[i])
+        {
+            dfs(i,-1,vis,adj,tin,low,bridges,timer);
+        }
+    }
+
+    for(auto &it : bridges)
+    {
+        cout<<it[0]<<" "<<it[1]<<endl;
+    }
+
+    return 0;
+}
